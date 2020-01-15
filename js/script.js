e466d6d53afd804402e92375039f218e73713ba9
Zayn Ejaz <50218403+zaynejaz@users.noreply.github.com>
2020-01-15 15:22:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,169 @@
+// PAGE LOAD ANIMATIONS | TWEENMAX
+
+// UNIVERSAL STARTS
+TweenMax.from(".navbar", 10, {
+  delay: 0,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeOut
+});
+
+TweenMax.from("footer", 10, {
+  delay: 1.2,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeOut
+});
+// UNIVERSAL ENDS
+
+// MAIN PAGE STARTS
+TweenMax.from(".header", 2, {
+  delay: 0,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+});
+// MAIN PAGE ENDS
+
+// WELCOME STARTS
+TweenMax.from(".welcome-bg", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".section-heading h1", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".section-heading p", 2, {
+  delay: 2.2,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".section-heading .res-btn", 1.5, {
+  delay: 2.5,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".map", 2, {
+  delay: 2.5,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeInOut
+});
+// WELCOME ENDS
+
+// MENU & WINE STARTS
+TweenMax.from(".description li a h1", 2, {
+  delay: 0,
+  opacity: 0,
+  y: -100,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from("section p", 2, {
+  delay: 1.8,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+// MENU & WINE ENDS
+
+// RESERVATIONS STARTS
+TweenMax.from(".bar, .dining-hall, .salon", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".res-container h1", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".res-container p", 2, {
+  delay: 2.2,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".res-container .res-btn", 1.5, {
+  delay: 2.5,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeInOut
+});
+// RESERVATIONS ENDS
+
+// TEAM STARTS
+TweenMax.from(".team", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".team-area", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: 75,
+  ease: Expo.easeInOut
+});
+// TEAM ENDS
+
+// PRIVATE DINING STARTS
+TweenMax.from(".private-dining, .west-dining, .north-south, .exclusive, .casa-events", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".pd-container h1, .pvtdn-container h2", 2, {
+  delay: 1.2,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".pd-container p", 2, {
+  delay: 2.2,
+  opacity: 0,
+  y: -50,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".pvtdn-container p", 2, {
+  delay: 2.2,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".pd-container .res-btn, .pvtdn-container .res-btn", 1.5, {
+  delay: 2.5,
+  opacity: 0,
+  y: 0,
+  ease: Expo.easeInOut
+});
+// PRIVATE DINING STARTS
+
+
+// FORM
+
+// FORM
+
+
